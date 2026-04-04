6c7db133f552108dd12e41a84abc390c487eb962
Linh Hoang <linhoang.ce@gmail.com>
2026-04-03 17:40:16 -0700

    remove dist

--- dist/middleware/validate.d.ts
+++
@@ -1,5 +0,0 @@
-import type { NextFunction, Request, Response } from "express";
-export declare const validateSession: (req: Request, res: Response, next: NextFunction) => void;
-export declare const validateAdmin: (req: Request, res: Response, next: NextFunction) => void;
-export declare const validateUser: (req: Request, res: Response, next: NextFunction) => void;
-//# sourceMappingURL=validate.d.ts.map
