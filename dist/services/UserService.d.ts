90a9f1429fcfda9c82e275e38e17f4defc16530f
linhoangce <37071697+linhoangce@users.noreply.github.com>
2026-04-03 17:40:53 -0700

    Delete dist directory

--- dist/services/UserService.d.ts
+++
@@ -1,8 +0,0 @@
-import DatabaseManager from "../lib/DatabaseManager.js";
-export declare class UserService {
-    private db;
-    constructor(db: DatabaseManager);
-    findUserByEmail(email: string): Promise<any>;
-    createNewUser(userData: any[]): Promise<import("mysql2").QueryResult>;
-}
-//# sourceMappingURL=UserService.d.ts.map
